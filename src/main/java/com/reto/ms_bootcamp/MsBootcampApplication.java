5b1124b8d40f15056da2da93cd50656adec89767
Santiago Quintero <santiago.quinteroc@pragma.com.co>
2026-01-29 09:55:24 -0500

    chore: init ms-bootcamp spring initializr

---
+++ src/main/java/com/reto/ms_bootcamp/MsBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.reto.ms_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto/ms_bootcamp/MsBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.ms_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
